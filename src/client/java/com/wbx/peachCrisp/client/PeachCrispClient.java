2a89c720b045a2912fdf3cdf53fe028b242ea1b3
WBX0513 <wbinxian513@163.com>
2026-07-17 13:46:48 +0800

    Add initial PeachCrispClient class implementation

---
+++ src/client/java/com/wbx/peachCrisp/client/PeachCrispClient.java
@@ -0,0 +1,10 @@
+package com.wbx.peachCrisp.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PeachCrispClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
