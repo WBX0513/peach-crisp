679efc8bba7a36f4d1c79fa492bbdd014b2ed6fc
WBX0513 <wbinxian513@163.com>
2026-08-19 19:04:20 +0800

    v1.2-pre-2

--- src/main/java/net/ygreenmc/mods/peachcrisp/PeachCrisp.java
+++ src/main/java/net/ygreenmc/mods/peachcrisp/PeachCrisp.java
@@ -2,9 +2,13 @@ package net.ygreenmc.mods.peachcrisp;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
+import net.fabricmc.fabric.api.loot.v3.FabricLootTableBuilder;
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.fabricmc.fabric.api.loot.v3.LootTableSource;
 import net.fabricmc.fabric.api.resource.v1.ResourceLoader;
 import net.fabricmc.fabric.api.resource.v1.pack.PackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
@@ -19,6 +23,10 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemUseAnimation;
 import net.minecraft.world.item.component.Consumable;
 import net.minecraft.world.item.consume_effects.ApplyStatusEffectsConsumeEffect;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -82,23 +90,50 @@ public class PeachCrisp implements ModInitializer {
         );
 
         ResourceLoader.registerBuiltinPack(
-            Identifier.fromNamespaceAndPath(MOD_ID, "balanced_diet_ext"),
-            FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow(),
-            PackActivationType.ALWAYS_ENABLED
-        );
-
-        ResourceLoader.registerBuiltinPack(
-            Identifier.fromNamespaceAndPath(MOD_ID, "peach_crisp_loot"),
-            FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow(),
-            PackActivationType.ALWAYS_ENABLED
-        );
-
-        ResourceLoader.registerBuiltinPack(
             Identifier.fromNamespaceAndPath(MOD_ID, "peach_crisp_trades"),
             FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow(),
             PackActivationType.ALWAYS_ENABLED
         );
 
+        registerPeachCrispLoot();
+
         LOGGER.info("Peach Crisp mod initialized!");
+    }
+
+    private static void registerPeachCrispLoot() {
+        LootTableEvents.MODIFY.register((key, tableBuilder, source, holder) -> {
+            if (source != LootTableSource.VANILLA) {
+                return;
+            }
+
+            switch (key.identifier().getPath()) {
+                case "chests/simple_dungeon" -> addPeachCrisp(tableBuilder, 0, 10, 1, 2);
+                case "chests/ancient_city" -> addPeachCrisp(tableBuilder, 0, 5, 2, 4);
+                case "chests/abandoned_mineshaft" -> addPeachCrisp(tableBuilder, 1, 10, 1, 2);
+                case "chests/desert_pyramid" -> addPeachCrisp(tableBuilder, 0, 15, 1, 3);
+                case "chests/village/village_desert_house" -> addPeachCrisp(tableBuilder, 0, 8, 2, 3);
+                case "chests/village/village_plains_house" -> addPeachCrisp(tableBuilder, 0, 8, 2, 3);
+                case "chests/village/village_savanna_house" -> addPeachCrisp(tableBuilder, 0, 8, 2, 3);
+                case "chests/village/village_snowy_house" -> addPeachCrisp(tableBuilder, 0, 8, 2, 3);
+                case "chests/village/village_taiga_house" -> addPeachCrisp(tableBuilder, 0, 8, 2, 3);
+                case "chests/village/village_temple" -> addPeachCrisp(tableBuilder, 0, 5, 2, 3);
+                default -> {
+                }
+            }
+        });
+    }
+
+    private static void addPeachCrisp(LootTable.Builder tableBuilder, int poolIndex, int weight, float minCount, float maxCount) {
+        int[] index = {0};
+        ((FabricLootTableBuilder) tableBuilder).modifyPools(poolBuilder -> {
+            if (index[0] == poolIndex) {
+                poolBuilder.add(
+                    LootItem.lootTableItem(PEACH_CRISP)
+                        .setWeight(weight)
+                        .apply(SetItemCountFunction.setCount(UniformGenerator.between(minCount, maxCount), false))
+                );
+            }
+            index[0]++;
+        });
     }
 }
